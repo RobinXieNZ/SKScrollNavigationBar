34a80cd3852ebd7deb26d107581a1f68ecc26441
RobinXieNZ <shengkunxie@gmail.com>
2016-03-21 09:14:11 +1300

    init setup

---
+++ SKScrollNavigationBarDemo/SKScrollNavigationBar/SKScrollNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  SKScrollNavigationBar.h
+//  SKScrollNavigationBarDemo
+//
+//  Created by Shengkun Xie on 16/3/21.
+//  Copyright © 2016年 TIME LIMITED. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKScrollNavigationBar : UIView
+
+@end
---
+++ SKScrollNavigationBarDemo/SKScrollNavigationBar/SKScrollNavigationBar.m
@@ -0,0 +1,21 @@
+//
+//  SKScrollNavigationBar.m
+//  SKScrollNavigationBarDemo
+//
+//  Created by Shengkun Xie on 16/3/21.
+//  Copyright © 2016年 TIME LIMITED. All rights reserved.
+//
+
+#import "SKScrollNavigationBar.h"
+
+@implementation SKScrollNavigationBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
